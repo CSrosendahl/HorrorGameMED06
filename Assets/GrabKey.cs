23cf2aebb420e8af09b4852acf3a84479b6d11cd
MariaL23 <91759715+MariaL23@users.noreply.github.com>
2024-04-19 14:27:44 +0200

    The Door Opens

--- Assets/GrabKey.cs
+++ Assets/GrabKey.cs
@@ -7,26 +7,36 @@ using UnityEngine.InputSystem;
 public class GrabKey : NetworkBehaviour
 {
     public GameObject keyOnHuman;
- 
+
     private InputAction eAction;
-   
+
+    private bool keyGrapped = false;
+
+    private GameObject collidedKey;
+
 
     private void Start()
     {
         keyOnHuman.SetActive(false);
-      
+
         // Get reference to the 'E' action
         eAction = GetComponent<PlayerInput>().actions["E"];
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object entering the trigger is tagged as "Human"
+        // Check if the object entering the trigger is tagged as "Key"
         if (other.CompareTag("Key"))
-        {   
-            // Add a listener to the 'E' action
-            eAction.performed += ctx => OnEPressed();
+        {
+            // Check if the key has not been grabbed yet
+            if (!keyGrapped)
+            {
+                collidedKey = other.gameObject;
+
+                // Add a listener to the 'E' action
+                eAction.performed += ctx => OnEPressed();
+            }
         }
     }
 
@@ -35,15 +45,23 @@ public class GrabKey : NetworkBehaviour
         // Check if the object exiting the trigger is tagged as "Key"
         if (other.CompareTag("Key"))
         {
-            // Remove the listener from the 'E' action
-            eAction.performed -= ctx => OnEPressed();
+            // Check if the key has been grabbed
+            if (keyGrapped)
+            {
+                // Remove the listener from the 'E' action
+                eAction.performed -= ctx => OnEPressed();
+            }
         }
     }
 
     private void OnEPressed()
     {
+        // Set keyGrapped to true to indicate that the key has been grabbed
+        keyGrapped = true;
+        Destroy(collidedKey);
+        // Activate the key on the player
         keyOnHuman.SetActive(true);
-      
-        Debug.Log("Key grabbed");
+       
+
     }
 }
