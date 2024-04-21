9db0e3247dc213295c44e20ddf05840ab30ef620
MariaL23 <91759715+MariaL23@users.noreply.github.com>
2024-04-21 20:27:43 +0200

    Fear me :cat:

--- Assets/OurFolder/Script/ScreamTrigger.cs
+++ Assets/OurFolder/Script/ScreamTrigger.cs
@@ -14,11 +14,11 @@ public class ScreamTrigger : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<PlayerManager>() != null)
+        if (other.GetComponent<PlayerManager>() != null)
         {
             PlayerManager playerManager = other.GetComponent<PlayerManager>();
 
-            if(playerManager.isHuman)
+            if (playerManager.isHuman)
             {
                 Debug.Log("Human was within the scream trigger");
                 // Do something here
@@ -29,18 +29,45 @@ public class ScreamTrigger : NetworkBehaviour
                         other.GetComponent<AffectedByScream>().ScreamEffect();
                         hasScreamed = true;
                         StartCoroutine(ScreamCoolDown());
+
+                        // Call the server RPC to trigger scream effect on other clients
+                        if (IsClient)
+                        {
+                            // Call the server RPC function directly
+                            ScreamOnClientsServerRPC();
+                        }
                     }
                 }
             }
-
-           
         }
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    void ScreamOnClientsServerRPC()
+    {
+        // This function will be executed on all clients by the server
+        RpcScreamEffectClientRPC();//Call RPC to trigger scream effect on all clients
+    }
+
+    [ClientRpc]
+    void RpcScreamEffectClientRPC()
+    {
+     
+        // Trigger scream effect here on all clients
+        
+        foreach (var playerGameObject in GameObject.FindGameObjectsWithTag("Human"))
+        {
+            var affectedByScream = playerGameObject.GetComponent<AffectedByScream>();
+            if (affectedByScream != null)
+            {
+                affectedByScream.ScreamEffect();
+            }
+        }
+    }
+
     IEnumerator ScreamCoolDown()
     {
-        
         yield return new WaitForSeconds(5f);
         hasScreamed = false;
-   
     }
 }
