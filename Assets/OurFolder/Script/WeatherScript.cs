6ab2bb0ed2b427f27093cf9fc8d1e2a343ba5ca2
CSrosendahl <csrpriv@gmail.com>
2024-04-22 12:45:38 +0200

    >Tikl maira

---
+++ Assets/OurFolder/Script/WeatherScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using Unity.Netcode;
+
+public class WeatherScript : NetworkBehaviour
+{
+    public Light directionalLight;
+    public float flashDuration = 0.1f;
+    public float flashIntensity = 100f;
+    public AudioSource thunderAudioSource;
+    public AudioSource rainAudioSource;
+    public AudioClip thunderClip;
+    public bool thunderPlayed;
+
+    public bool playWeatherEffects;
+    private void Start()
+    {
+        playWeatherEffects = true;
+        thunderPlayed = false;
+        StartCoroutine(RandomLightningStrike());
+    }
+
+    public void LightningStrike()
+    {
+        StartCoroutine(FlashLight());
+    }
+
+    IEnumerator FlashLight()
+    {
+        directionalLight.intensity = flashIntensity;
+
+        if(!thunderPlayed)
+        {
+            thunderAudioSource.clip = thunderClip;
+            thunderAudioSource.Play();
+        }
+        
+        yield return new WaitForSeconds(flashDuration);
+        directionalLight.intensity = 1;
+    }
+
+    IEnumerator RandomLightningStrike()
+    {
+    
+        while (playWeatherEffects)
+        {
+            // Wait for a random interval between 5 and 20 seconds
+            float randomInterval = Random.Range(6f, 20f);
+            yield return new WaitForSeconds(randomInterval);
+
+            // Perform a lightning strike with two quick flashes
+            LightningStrike();
+            thunderPlayed = true;
+            yield return new WaitForSeconds(1);
+            LightningStrike();
+            thunderPlayed = false;
+        }
+    }
+}
