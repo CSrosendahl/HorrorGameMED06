dea8c082640443c4b72aa455b608110a4035413f
MariaL23 <91759715+MariaL23@users.noreply.github.com>
2024-04-23 21:42:54 +0200

    Still chest animation

--- Assets/ChestOpener.cs
+++ Assets/ChestOpener.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-
 public class ChestOpener : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator; // Reference to the animator component
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the object entering the collider is tagged as "Human"
+        if (other.CompareTag("Human"))
+        {
+            // Check if the "F" key is pressed
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                // Call the OpenChest method on the server
+                OpenChestServerRpc();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method to open the chest on the server and synchronize the animation
+    [ServerRpc]
+    void OpenChestServerRpc()
     {
-        
+        // Trigger the animation on the server
+        OpenChestClientRpc();
+    }
+
+    // Method to open the chest on all clients and synchronize the animation
+    [ClientRpc]
+    void OpenChestClientRpc()
+    {
+        // Trigger the animation on all clients
+        animator.SetTrigger("Open"); // Assumes you have a trigger parameter named "Open" in your animator controller
     }
 }
