f0798b3b2f8d614c3a3517f154531995056bb2ae
CSrosendahl <csrpriv@gmail.com>
2024-04-25 21:24:18 +0200

    321

--- Assets/InfoUIScript.cs
+++ Assets/InfoUIScript.cs
@@ -18,24 +18,29 @@ public class InfoUIScript : MonoBehaviour
         // Check if the PlayerManager script exists
        
             // Check if the player is human or monster and show the corresponding UI
-            if (humanManager.isHuman)
-            {
-                humanUI.SetActive(true);
-                monsterUI.SetActive(false);
-                Debug.Log("Human UI Active");
-            }
-            else if (monsterManager.isMonster)
-            {
-                humanUI.SetActive(false);
-                monsterUI.SetActive(true);
-            }
-            else
-            {
-                // If neither is true, hide both UIs
-                humanUI.SetActive(false);
-                monsterUI.SetActive(false);
-            }
+      
         
        
+    }
+
+    private void Update()
+    {
+        if (humanManager.isHuman)
+        {
+            humanUI.SetActive(true);
+            monsterUI.SetActive(false);
+            Debug.Log("Human UI Active");
+        }
+        else if (monsterManager.isMonster)
+        {
+            humanUI.SetActive(false);
+            monsterUI.SetActive(true);
+        }
+        else
+        {
+            // If neither is true, hide both UIs
+            humanUI.SetActive(false);
+            monsterUI.SetActive(false);
+        }
     }
 }
